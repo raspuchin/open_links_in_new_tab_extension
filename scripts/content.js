456976947644e240da44ddbf7819e148dffa05b1
Sachin Pothukuchi <sachin.pothukuchi@gmail.com>
2023-05-18 19:32:18 -0700

    created the app and commit

---
+++ scripts/content.js
@@ -0,0 +1,41 @@
+document.onreadystatechange = function () {
+  function isOriginSameAsLocation(url) {
+    var pageLocation = window.location;
+    var URL_HOST_PATTERN = /(\w+:)?(?:\/\/)([\w.-]+)?(?::(\d+))?\/?/;
+    var urlMatch = URL_HOST_PATTERN.exec(url) || [];
+    var urlparts = {
+      protocol: urlMatch[1] || "",
+      host: urlMatch[2] || "",
+      port: urlMatch[3] || "",
+    };
+
+    function defaultPort(protocol) {
+      return { "http:": 80, "https:": 443 }[protocol];
+    }
+
+    function portOf(location) {
+      return (
+        location.port || defaultPort(location.protocol || pageLocation.protocol)
+      );
+    }
+
+    return !!(
+      urlparts.protocol &&
+      urlparts.protocol == pageLocation.protocol &&
+      urlparts.host &&
+      urlparts.host == pageLocation.host &&
+      urlparts.host &&
+      portOf(urlparts) == portOf(pageLocation)
+    );
+  }
+
+  const a = document.querySelectorAll("a");
+
+  if (a) {
+    for (var i = 0; i < a.length; i++) {
+      if (! isOriginSameAsLocation(a[i].href)) {
+        a[i].target = "_blank";
+      }
+    }
+  }
+};
